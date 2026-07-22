dc49de860a76adbe963667f6bb5dd0ac9487000f
shubha-2018 <thoratshubha2018@gmail.com>
2026-07-22 19:28:10 +0530

    Fixed database port and SSL for Aiven

--- Mazi-City-Backend/config/db.js
+++ Mazi-City-Backend/config/db.js
@@ -6,6 +6,8 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
+  ssl: { rejectUnauthorized: false }
 });
 
 module.exports = db;
